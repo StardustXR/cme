e875f7586240319f2e8e320f1be82d16d55abb77
Schmarni <marnistromer@gmail.com>
2026-03-10 22:17:21 +0100

    feat: add srgb variants to to_drm_fourcc
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- src/format.rs
+++ src/format.rs
@@ -117,21 +117,25 @@ impl VulkanoFormatExtension for Format {
             F::R5G5B5A1_UNORM_PACK16 => &[D::Abgr1555, D::Xbgr1555, D::Rgba5551, D::Rgbx5551],
             F::A2B10G10R10_UNORM_PACK32 => &[D::Abgr2101010, D::Xbgr2101010],
             F::A4B4G4R4_UNORM_PACK16 => &[D::Abgr4444, D::Xbgr4444],
-            F::R8G8B8A8_UNORM => &[D::Abgr8888, D::Xbgr8888, D::Rgba8888, D::Rgbx8888],
+            F::R8G8B8A8_UNORM | F::R8G8B8A8_SRGB => {
+                &[D::Abgr8888, D::Xbgr8888, D::Rgba8888, D::Rgbx8888]
+            }
             F::A1R5G5B5_UNORM_PACK16 => &[D::Argb1555, D::Xrgb1555],
             F::A2R10G10B10_UNORM_PACK32 => &[D::Argb2101010, D::Xrgb2101010],
             F::B4G4R4A4_UNORM_PACK16 => &[D::Argb4444, D::Xrgb4444, D::Bgra4444, D::Bgrx4444],
-            F::B8G8R8A8_UNORM => &[D::Argb8888, D::Xrgb8888, D::Bgra8888, D::Bgrx8888],
+            F::B8G8R8A8_UNORM | F::B8G8R8A8_SRGB => {
+                &[D::Argb8888, D::Xrgb8888, D::Bgra8888, D::Bgrx8888]
+            }
             F::B5G6R5_UNORM_PACK16 => &[D::Bgr565],
-            F::B8G8R8_UNORM => &[D::Bgr888],
+            F::B8G8R8_UNORM | F::B8G8R8_SRGB => &[D::Bgr888],
             // D::Bgr888_a8 => F::B8G8R8A8_UNORM,
             F::B5G5R5A1_UNORM_PACK16 => &[D::Bgra5551, D::Bgrx5551],
             F::R16_UNORM => &[D::R16],
-            F::R8_UNORM => &[D::R8],
+            F::R8_UNORM | F::R8_SRGB => &[D::R8],
             F::R16G16_UNORM => &[D::Rg1616],
-            F::R8G8_UNORM => &[D::Rg88],
+            F::R8G8_UNORM | F::R8G8_SRGB => &[D::Rg88],
             F::R5G6B5_UNORM_PACK16 => &[D::Rgb565],
-            F::R8G8B8_UNORM => &[D::Rgb888],
+            F::R8G8B8_UNORM | F::R8G8B8_SRGB => &[D::Rgb888],
             // D::Rgb888_a8 => F::R8G8B8A8_UNORM,
             F::R4G4B4A4_UNORM_PACK16 => &[D::Rgba4444, D::Rgbx4444],
             F::R16G16B16A16_SFLOAT => &[D::Abgr16161616f],
